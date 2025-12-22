c5077f4315830ee3c377f47be921545a9ef43582
Dheemanth07 <dheemanth1007@gmail.com>
2025-12-22 21:16:56 +0530

    created authentication middleware to verify jwt

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,89 +1,115 @@
-import bcrypt from 'bcrypt';
-import prisma from '../prisma.js';
-import { signinToken } from '../utils/jwt.js';
+import bcrypt from "bcrypt";
+import prisma from "../prisma.js";
+import { signinToken } from "../utils/jwt.js";
 
-export const register = async (req, res)=>{
-    try{
-
+export const register = async (req, res) => {
+    try {
         // Validate request body
         if (!req.body) {
             return res.status(400).json({ message: "Request body is missing" });
         }
 
-        const {name,email,password}=req.body;
+        const { name, email, password } = req.body;
 
         // 1. Check if all fields are provided
-        if(!name || !email ||!password){
-            return res.status(400).json({message:"All fields are required"});
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
         }
 
         // 2. Check if password is at least 6 characters long
-        if(password.length<6){
-            return res.status(400).json({message:"Password must be at least 6 characters long"});
+        if (password.length < 6) {
+            return res
+                .status(400)
+                .json({
+                    message: "Password must be at least 6 characters long",
+                });
         }
 
         // 3. Check if user already exists
-        const existingUser = await prisma.user.findUnique({where:{email}});
-        if(existingUser){
-            return res.status(400).json({message:"User already exists with this email"});
+        const existingUser = await prisma.user.findUnique({ where: { email } });
+        if (existingUser) {
+            return res
+                .status(400)
+                .json({ message: "User already exists with this email" });
         }
 
         // 4. For new user Hash the password with 'bcrypt' and salt rounds of 10
-        const hashedPassword = await bcrypt.hash(password,10);
+        const hashedPassword = await bcrypt.hash(password, 10);
 
         // 5. Create new user in the database
         const user = await prisma.user.create({
-            data:{
+            data: {
                 name,
                 email,
-                password:hashedPassword,
-            
+                password: hashedPassword,
             },
         });
 
         // 6. Return success response
-        return res.status(201).json({message:"User registered successfully", user});
-    }catch(err){
+        return res
+            .status(201)
+            .json({ message: "User registered successfully", user });
+    } catch (err) {
         console.error("Error during registration:", err);
-        return res.status(500).json({message:"Internal server error"});
+        return res.status(500).json({ message: "Internal server error" });
     }
-}
+};
 
-export const login = async (req,res)=>{
-    try{
+export const login = async (req, res) => {
+    try {
         // Validate request body
-        if(!req.body){
-            return res.status(400).json({message:"Request body is missing"});
+        if (!req.body) {
+            return res.status(400).json({ message: "Request body is missing" });
         }
 
-        const{email,password}=req.body;
+        const { email, password } = req.body;
 
         // 1. Check if all fields are provided
-        if(!email || !password){
-            return res.status(400).json({message:"All fields are required"});
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
         }
 
         // 2. Check if user exists
-        const user = await prisma.user.findUnique({where:{email}});
+        const user = await prisma.user.findUnique({ where: { email } });
 
-        if(!user){
-            return res.status(401).json({message:"User does not exist with email"});
+        if (!user) {
+            return res
+                .status(401)
+                .json({ message: "User does not exist with email" });
         }
 
         // 3. Compare provided password with stored hashed password
-        const isMatch = await bcrypt.compare(password,user.password);
+        const isMatch = await bcrypt.compare(password, user.password);
 
-        if(!isMatch){
-            return res.status(401).json({message:"Invalid password"});
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid password" });
         }
 
         // 4. Generate JWT token
-        const token = signinToken({id:user.id,email:user.email});
+        const token = signinToken({ id: user.id, email: user.email });
 
         // 5. Return success response with token
-        return res.status(200).json({message:"Login successful", token,user});
-    }catch(err){
+        return res
+            .status(200)
+            .json({ message: "Login successful", token, user });
+    } catch (err) {
         console.error("Login error: ", err);
-        return res.status(500).json({message:"Internal server error"});
+        return res.status(500).json({ message: "Internal server error" });
     }
-}
+};
+
+export const getMe = async (req, res) => {
+    try {
+        const userId = req.user.id;
+
+        const user = await prisma.user.findUnique({
+            where: { id: userId },
+            select: { id: true, name: true, email: true,createdAt:true},
+        });
+
+        return res.status(200).json({ user });
+    } catch (err) {
+        console.error("Error retrieving user details:", err);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = (req, res, next) => {
+    try {
+        // 1. Get authorization header
+        const authHeader = req.headers.authorization;
+
+        // 2. Check if header is present and starts with 'Bearer '
+        if (!authHeader || !authHeader.startsWith("Bearer "))
+            return res
+                .status(401)
+                .json({ message: "Authorizatoin header missing" });
+
+        // 3. Extract token
+        const token = authHeader.split(" ")[1];
+
+        // 4. Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        // 5. Attach user info to request object
+        req.user = decoded;
+
+        // 6. Proceed to next middleware/controller
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: "Invalid token" });
+    }
+};
